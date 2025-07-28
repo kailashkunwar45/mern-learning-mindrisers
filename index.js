a281f3cb7dcfa93197483a980649533e09d3d068
Kailash Kunwar <kailashkunwar10@gmail.com>
2025-07-27 17:06:39 -0700

    changed

--- index.js
+++ index.js
@@ -1,25 +1,86 @@
-import express from 'express';
+// import express from 'express';
+
+// const app = express();
+// const port = 5000;
+
+
+// app.get('/', (req, res) => {
+//   console.log(req.query);
+//   const num = Number(req.query.number);
+
+//   if (isNaN(num)) {
+//     return res.status(400).json({
+//       error: 'wrong Input'
+//     })
+//   }
+
+//   const square = num * num;
+//   res.json({ number: num, square });
+
+// });
+
+
+// app.listen(port, () => {
+//   console.log('server is running');
+// });
+
+//my code
+
+
+// import express from 'express';
+
+// const app = express();
+// const port = 5000;
+
+// app.use(express.json());
+
+// app.use('/', (req, res, next) => {
+//   const { c } = req.query;
+
+//   if (isNaN(Number(c))) {
+//     return res.status(400).json({
+//       error: 'wrong input'
+//     });
+//   }
+
+//   next();
+// });
+
+// app.get('/', (req, res) => {
+//   console.log(req.body);
+//   return res.status(200).json({ message: 'k ma hency haina rwwww paraaaaaaa' });
+
+
+// })
+
+// app.get('/', (req, res) => {
+//   const { c } = req.query;
+//   return res.status(200).json({
+//     message: Number(c) * Number(c)
+//   });
+// });
+
+// app.listen(port, () => {
+//   console.log('server is running');
+
+// });
+
+
+
+import express from "express";
 
 const app = express();
 const port = 5000;
 
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   console.log(req.query);
-  const num = Number(req.query.number);
-
-  if (isNaN(num)) {
-    return res.status(400).json({
-      error: 'wrong Input'
-    })
+  const c = String(req.query.c);
+  if (!c.trim() || !/^[a-zA-Z\s]+$/.test(c.trim())) {
+    return res.status(400).json({ error: "bhayena" });
   }
 
-  const square = num * num;
-  res.json({ number: num, square });
-
+  res.json({ message: c.trim().toUpperCase() });
 });
 
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
-app.listen(port, () => {
-  console.log('server is running');
-});
